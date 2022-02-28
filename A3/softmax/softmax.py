c0b4d0d4364c970d711b82fc100b84e050a7f864
Mikerpen22 <b04502053@ntu.edu.tw>
2022-02-28 00:37:07 -0600

    add implementations for softmax.py

--- A3/softmax/softmax.py
+++ A3/softmax/softmax.py
@@ -1,33 +1,59 @@
+from matplotlib.pyplot import axis
 import numpy as np
 from random import shuffle
 import scipy.sparse
 
+
 def softmax_loss_naive(theta, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
-  Inputs:
-  - theta: d x K parameter matrix. Each column is a coefficient vector for class k
-  - X: m x d array of data. Data are d-dimensional rows.
-  - y: 1-dimensional array of length m with labels 0...K-1, for K classes
-  - reg: (float) regularization strength
-  Returns:
-  a tuple of:
-  - loss as single float
-  - gradient with respect to parameter matrix theta, an array of same size as theta
-  """
-  # Initialize the loss and gradient to zero.
+    """
+    Softmax loss function, naive implementation (with loops)
+    Inputs:
+    - theta: d x K parameter matrix. Each column is a coefficient vector for class k
+    - X: m x d array of data. Data are d-dimensional rows.
+    - y: 1-dimensional array of length m with labels 0...K-1, for K classes
+    - reg: (float) regularization strength
+    Returns:
+    a tuple of:
+    - loss as single float
+    - gradient with respect to parameter matrix theta, an array of same size as theta
+    """
+    # Initialize the loss and gradient to zero.
 
     J = 0.0
     grad = np.zeros_like(theta)
     m, dim = X.shape
-
+    K = theta.shape[1]
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.     #
     # Store the loss in J and the gradient in grad. If you are not              #
     # careful here, it is easy to run into numeric instability. Don't forget    #
     # the regularization term!                                                  #
     #############################################################################
+    # Loss calculation:
+    pred = np.dot(X, theta)    # (m, K)
+    # (m, 1) <- max will collapse the other dim, so add 1 to it
+    max_pred_element = np.max(pred, axis=1).reshape((-1, 1))
+    pred -= max_pred_element
+    pred = np.exp(pred)
+    pred_sum = np.sum(pred, axis=1).reshape((-1, 1))    # (m, 1)
+    pred /= pred_sum    # (m, K)
+    for i in range(m):
+        for k in range(K):
+            if(y[i] == k):
+                J += np.log(pred[i, k])
+    J /= (-m)
+    J += reg * np.sum(np.power(theta, 2)) / 2 / m
 
+    # Gradient calculation: -> grad shape (d, K)
+    for k in range(K):
+        for i in range(m):
+            x_i = X[i, :]  # shape: (1, d)
+            if y[i] == k:
+                grad[:, k] += (x_i * (1 - pred[i, k]))  # shape: (1, d)
+            else:
+                grad[:, k] += (x_i * (0 - pred[i, k]))
+        grad[:, k] /= (-m)
+        grad[:, k] += reg * (theta[:, k])/m
 
     #############################################################################
     #                          END OF YOUR CODE                                 #
@@ -35,26 +61,44 @@ def softmax_loss_naive(theta, X, y, reg):
 
     return J, grad
 
-  
+
 def softmax_loss_vectorized(theta, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
+    """
+    Softmax loss function, vectorized version.
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
 
     J = 0.0
     grad = np.zeros_like(theta)
     m, dim = X.shape
-
+    K = theta.shape[1]
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
     # Store the loss in J and the gradient in grad. If you are not careful      #
     # here, it is easy to run into numeric instability. Don't forget the        #
     # regularization term!                                                      #
     #############################################################################
+    pred = np.dot(X, theta)    # (m, K)
+    # (m, 1) <- max will collapse the other dim, so add 1 to it
+    max_pred_element = np.max(pred, axis=1).reshape((-1, 1))
+    pred -= max_pred_element
+    pred = np.exp(pred)
+    pred_sum = np.sum(pred, axis=1).reshape((-1, 1))    # (m, 1)
+    pred /= pred_sum    # (m, K)
 
+    # np.eye(number of classes)[vector containing the labels] -> cool!
+    onehot = np.eye(K)[y]  # one-hot encoded label matrix -> (m, k)
+    # A*B -> row by row multiplication
+    J = -np.sum(onehot * np.log(pred))/m
+    J += reg * np.sum(np.square(theta)) / 2.0 / m
 
+    # grad shape: (d, K)
+    # onehot - pred: (m, k)
+    # X: (m, d)
+    # theta: (d, K)
+
+    grad = (np.matmul(X.T, (onehot-pred))/(-m)) + (reg * theta / m)
     #############################################################################
     #                          END OF YOUR CODE                                 #
     #############################################################################
--- A3/softmax/utils.py
+++ A3/softmax/utils.py
@@ -4,50 +4,52 @@ import numpy as np
 import os
 import _pickle as pickle
 
-def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
-  """
-  Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
-  it for the softmax classifier. 
-  """
-  # Load the raw CIFAR-10 data
 
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the softmax classifier.
+    """
+    # Load the raw CIFAR-10 data
     cifar10_dir = 'datasets/cifar-10-batches-py'
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-  
-  # subsample the data
 
-    X_train, y_train, X_val, y_val, X_test, y_test = subsample(num_training,num_validation,num_test,X_train,y_train,X_test,y_test)
+  # subsample the data
+    X_train, y_train, X_val, y_val, X_test, y_test = subsample(
+        num_training, num_validation, num_test, X_train, y_train, X_test, y_test)
 
   # visualize a subset of the training data
-  
-    visualize_cifar10(X_train,y_train)
+
+    visualize_cifar10(X_train, y_train)
 
   # preprocess data
-  
+
     X_train, X_val, X_test = preprocess(X_train, X_val, X_test)
-  
+
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
 def load_CIFAR_batch(filename):
-  """ load single batch of cifar """
-  with open(filename, 'rb') as f:
-    datadict = pickle.load(f,encoding='latin1')
-    X = datadict['data']
-    Y = datadict['labels']
-    X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
-    Y = np.array(Y)
-    return X, Y
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = pickle.load(f, encoding='latin1')
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1).astype(
+            "float")  # interesting usage of transpose!
+        Y = np.array(Y)
+        return X, Y
+
 
 def load_CIFAR10(cifar10_root):
-  """ load all of cifar """
+    """ load all of cifar """
     xs = []
     ys = []
-    for b in range(1,6):
+    for b in range(1, 6):
         f = os.path.join(cifar10_root, 'data_batch_%d' % (b, ))
         X, Y = load_CIFAR_batch(f)
         xs.append(X)
-        ys.append(Y)    
+        ys.append(Y)
     Xtr = np.concatenate(xs)
     Ytr = np.concatenate(ys)
     del X, Y
@@ -58,8 +60,9 @@ def load_CIFAR10(cifar10_root):
 # Visualize some examples from the dataset.
 # We show a few examples of training images from each class.
 
-def visualize_cifar10(X_train,y_train):
-    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+def visualize_cifar10(X_train, y_train):
+    classes = ['plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
     num_classes = len(classes)
     samples_per_class = 7
     for y, cls in enumerate(classes):
@@ -77,70 +80,72 @@ def visualize_cifar10(X_train,y_train):
 
 # subsampling  the data
 
-def subsample(num_training,num_validation,num_test,X_train,y_train,X_test,y_test):
 
-  # Our validation set will be num_validation points from the original
-  # training set.
+def subsample(num_training, num_validation, num_test, X_train, y_train, X_test, y_test):
 
-  mask = range(num_training, num_training + num_validation)
-  X_val = X_train[mask]
-  y_val = y_train[mask]
+    # Our validation set will be num_validation points from the original
+    # training set.
 
-  # Our training set will be the first num_train points from the original
-  # training set.
-  mask = range(num_training)
-  X_train = X_train[mask]
-  y_train = y_train[mask]
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
 
-  # We use the first num_test points of the original test set as our
-  # test set.
+    # Our training set will be the first num_train points from the original
+    # training set.
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
 
-  mask = range(num_test)
-  X_test = X_test[mask]
-  y_test = y_test[mask]
+    # We use the first num_test points of the original test set as our
+    # test set.
 
-  return X_train, y_train, X_val, y_val, X_test, y_test
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
 
-def preprocess(X_train,X_val,X_test):
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
-  # Preprocessing: reshape the image data into rows
+
+def preprocess(X_train, X_val, X_test):
+
+    # Preprocessing: reshape the image data into rows (flatten 32*32*3 -> 3073)
 
     X_train = np.reshape(X_train, (X_train.shape[0], -1))
     X_val = np.reshape(X_val, (X_val.shape[0], -1))
     X_test = np.reshape(X_test, (X_test.shape[0], -1))
 
-  # As a sanity check, print out the shapes of the data
-    print('Training data shape: %d,%d' %X_train.shape)
-    print('Validation data shape: %d,%d' %X_val.shape)
-    print('Test data shape: %d,%d' %X_test.shape)
+    # As a sanity check, print out the shapes of the data
+    print('Training data shape: %d,%d' % X_train.shape)
+    print('Validation data shape: %d,%d' % X_val.shape)
+    print('Test data shape: %d,%d' % X_test.shape)
 
-  # Preprocessing: subtract the mean image
-  # first: compute the image mean based on the training data
+    # Preprocessing: subtract the mean image
+    # first: compute the image mean based on the training data
 
     mean_image = np.mean(X_train, axis=0)
 #  plt.figure(figsize=(4,4))
 #  plt.imshow(mean_image.reshape((32,32,3)).astype('uint8')) # visualize the mean image
 
-  # second: subtract the mean image from train and test data
+    # second: subtract the mean image from train and test data
 
     X_train -= mean_image
     X_val -= mean_image
     X_test -= mean_image
 
-  # third: append the bias dimension of ones (i.e. bias trick) so that our softmax regressor
-  # only has to worry about optimizing a single weight matrix theta.
-  # Also, lets transform data matrices so that each image is a row.
+    # third: append the bias dimension of ones (i.e. bias trick) so that our softmax regressor
+    # only has to worry about optimizing a single weight matrix theta.
+    # Also, lets transform data matrices so that each image is a row.
 
     X_train = np.hstack([np.ones((X_train.shape[0], 1)), X_train])
     X_val = np.hstack([np.ones((X_val.shape[0], 1)), X_val])
     X_test = np.hstack([np.ones((X_test.shape[0], 1)), X_test])
 
-    print('Training data shape with bias term: %d,%d' %X_train.shape)
-    print('Validation data shape with bias term: %d,%d' %X_val.shape)
-    print('Test data shape with bias term: %d,%d' %X_test.shape)
+    print('Training data shape with bias term: %d,%d' % X_train.shape)
+    print('Validation data shape with bias term: %d,%d' % X_val.shape)
+    print('Test data shape with bias term: %d,%d' % X_test.shape)
 
     return X_train, X_val, X_test
-  
+
 
 def sigmoid(X):
     return 1./(1. + np.exp(-X))
