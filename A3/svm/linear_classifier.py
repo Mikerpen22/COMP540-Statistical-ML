95a9ec8b8a6609c9f20fef1d39ef807c137e3704
Mikerpen22 <b04502053@ntu.edu.tw>
2022-03-01 00:01:07 -0600

    add implementations for svm classifier & cross validations for params tuning

--- A3/svm/linear_classifier.py
+++ A3/svm/linear_classifier.py
@@ -1,118 +1,117 @@
+from linear_svm import svm_loss_vectorized
 import numpy as np
+
 
 class LinearClassifier(object):
 
-  def __init__(self):
-    self.theta = None
+    def __init__(self):
+        self.theta = None
 
-  def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100,batch_size=200,
-             verbose=False):
-    """
-    Train this linear classifier using gradient descent.
+    def train(self, X, y, learning_rate=1e-3, reg=1e-5, num_iters=100, batch_size=200,
+              verbose=False):
+        """
+        Train this linear classifier using gradient descent.
 
-    Inputs:
-    - X: A numpy array of shape (m, d) containing training data; there are m
-      training samples each of dimension d.
-    - y: A numpy array of shape (m,) containing training labels; y[i] = -1,1 for two class problems
-    - learning_rate: (float) learning rate for optimization.
-    - reg: (float) regularization strength
-    - num_iters: (integer) number of steps to take when optimizing
-    - batch_size: (integer) number of training examples to use at each step. 
-    - verbose: (boolean) If true, print progress during optimization.
+        Inputs:
+        - X: A numpy array of shape (m, d) containing training data; there are m
+                training samples each of dimension d.
+        - y: A numpy array of shape (m,) containing training labels; y[i] = -1,1 for two class problems
+        - learning_rate: (float) learning rate for optimization.
+        - reg: (float) regularization strength
+        - num_iters: (integer) number of steps to take when optimizing
+        - batch_size: (integer) number of training examples to use at each step. 
+        - verbose: (boolean) If true, print progress during optimization.
 
-    Outputs:
-    A list containing the value of the loss function at each training iteration.
-    """
-    m,d = X.shape
-    K  = np.max(y) + 1 # assume y takes values 0...K-1 where K is number of classes
+        Outputs:
+        A list containing the value of the loss function at each training iteration.
+        """
+        m, d = X.shape
+        # assume y takes values 0...K-1 where K is number of classes
+        K = np.max(y) + 1
 
-    if self.theta is None:
-      # lazily initialize theta
-      if K == 2:
-        self.theta = 0.001 * np.random.randn((d,))
-      else:
-        self.theta = 0.001 * np.random.randn(d, K)
+        if self.theta is None:
+            # lazily initialize theta
+            if K == 2:
+                self.theta = 0.001 * np.random.randn((d,))
+            else:
+                self.theta = 0.001 * np.random.randn(d, K)
 
-    # Run gradient descent to optimize theta
-    loss_history = []
-    for it in range(num_iters):
+        # Run gradient descent to optimize theta
+        loss_history = []
+        for it in range(num_iters):
 
-      # draw batch_size examples from X
-      if batch_size < X.shape[0]:
-        rand_idx = np.random.choice(m, batch_size)
-        X_batch = X[rand_idx,:]
-        y_batch = y[rand_idx]
-      else:
-        X_batch = X
-        y_batch = y
+            # draw batch_size examples from X
+            if batch_size < X.shape[0]:
+                rand_idx = np.random.choice(m, batch_size)
+                X_batch = X[rand_idx, :]
+                y_batch = y[rand_idx]
+            else:
+                X_batch = X
+                y_batch = y
 
-      # evaluate loss and gradient
-      loss, grad = self.loss(X_batch, y_batch, reg)
-      loss_history.append(loss)
+            # evaluate loss and gradient
+            loss, grad = self.loss(X_batch, y_batch, reg)
+            loss_history.append(loss)
 
-      # perform parameter update
-      self.theta = self.theta - learning_rate * grad
+            # perform parameter update
+            self.theta = self.theta - learning_rate * grad
 
-      if verbose and it % 100 == 0:
-        print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+            if verbose and it % 100 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
-    return loss_history
+        return loss_history
 
-  def predict(self, X):
-    """
-    Use the coefficients of this linear classifier to predict labels for
-    data points.
+    def predict(self, X):
+        """
+        Use the coefficients of this linear classifier to predict labels for
+        data points.
 
-    Inputs:
-    - X: m x d array of training data. Each row is a d-dimensional point.
+        Inputs:
+        - X: m x d array of training data. Each row is a d-dimensional point.
 
-    Returns:
-    - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-      array of length m, and each element is an integer giving the
-      predicted class.
-    """
-    pass
-  
-  def loss(self, X, y, reg):
-    """
-    Compute the loss function and its derivative. 
-    Subclasses will override this.
+        Returns:
+        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
+                array of length m, and each element is an integer giving the
+                predicted class.
+        """
+        pass
 
-    Inputs:
-    - X: A numpy array of shape (m,d)
-    - y: A numpy array of shape (m,) containing labels for X.
+    def loss(self, X, y, reg):
+        """
+        Compute the loss function and its derivative. 
+        Subclasses will override this.
 
-    - reg: (float) penalty term.
+        Inputs:
+        - X: A numpy array of shape (m,d)
+        - y: A numpy array of shape (m,) containing labels for X.
 
-    Returns: A tuple containing:
-    - loss as a single float
-    - gradient with respect to self.theta; an array of the same shape as theta
-    """
-    pass
+        - reg: (float) penalty term.
 
-from linear_svm import svm_loss_vectorized
+        Returns: A tuple containing:
+        - loss as a single float
+        - gradient with respect to self.theta; an array of the same shape as theta
+        """
+        pass
 
 
 class LinearSVM(LinearClassifier):
-  """ A subclass that uses the Multiclass SVM loss function """
+    """ A subclass that uses the Multiclass SVM loss function """
 
-  def loss(self, X, y, reg):
-    return svm_loss_vectorized(self.theta, X, y, reg)
+    def loss(self, X, y, reg):
+        return svm_loss_vectorized(self.theta, X, y, reg)
 
-  def predict(sellf,X):
-    y_pred = np.zeros(X.shape[0])
+    def predict(self, X):
+        y_pred = np.zeros(X.shape[0])
 
-    ##################################################################
-    #   TODO: SVM prediction function, use X and self.theta          #
-    #   return y_pred with class associated with each row of X       #
-    #   1-2 lines of code expected                                   #
-    ##################################################################
+        ##################################################################
+        #   TODO: SVM prediction function, use X and self.theta          #
+        #   return y_pred with class associated with each row of X       #
+        #   1-2 lines of code expected                                   #
+        ##################################################################
+        y_pred = np.argmax(np.dot(X, self.theta), axis=1)  # (m, k)
 
+        ##################################################################
+        #   END OF YOUR CODE                                             #
+        ##################################################################
 
-    ##################################################################
-    #   END OF YOUR CODE                                             #
-    ##################################################################
-
-    
-    return y_pred
-    
+        return y_pred
--- A3/svm/linear_svm.py
+++ A3/svm/linear_svm.py
@@ -8,90 +8,123 @@ import numpy as np
 # SVM multiclass
 
 def svm_loss_naive(theta, X, y, reg):
-  """
-  Structured SVM loss function, naive implementation (with loops).
+    """
+    Structured SVM loss function, naive implementation (with loops).
 
-  Inputs have dimension d, there are K classes, and we operate on minibatches
-  of m examples.
+    Inputs have dimension d, there are K classes, and we operate on minibatches
+    of m examples.
 
-  Inputs:
-  - theta: A numpy array of shape d X K containing parameters.
-  - X: A numpy array of shape m X d containing a minibatch of data.
-  - y: A numpy array of shape (m,) containing training labels; y[i] = k means
-    that X[i] has label k, where 0 <= k < K.
-  - reg: (float) regularization strength
+    Inputs:
+    - theta: A numpy array of shape d x K containing parameters.
+    - X: A numpy array of shape m x d containing a minibatch of data.
+    - y: A numpy array of shape (m,) containing training labels; y[i] = k means
+            that X[i] has label k, where 0 <= k < K.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss J as single float
-  - gradient with respect to weights theta; an array of same shape as theta
-  """
+    Returns a tuple of:
+    - loss J as single float
+    - gradient with respect to weights theta; an array of same shape as theta
+    """
 
-  K = theta.shape[1] # number of classes
-  m = X.shape[0]     # number of examples
+    K = theta.shape[1]  # number of classes
+    m = X.shape[0]     # number of examples
 
-  J = 0.0
-  dtheta = np.zeros(theta.shape) # initialize the gradient as zero
-  delta = 1.0
+    J = 0.0
+    # initialize the gradient as zero, shape = (d, K)
+    dtheta = np.zeros(theta.shape)
+    delta = 1.0
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the loss function and store it in J.                              #
-  # Do not forget the regularization term!                                    #
-  # code above to compute the gradient.                                       #
-  # 8-10 lines of code expected                                               #
-  #############################################################################
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the loss function and store it in J.                              #
+    # Do not forget the regularization term!                                    #
+    # code above to compute the gradient.                                       #
+    # 8-10 lines of code expected                                               #
+    #############################################################################
+    pred = np.dot(X, theta)  # (m, k)
 
+    for i in range(m):
+        pred_i = pred[i]  # (1, k)
+        margins = pred_i - pred[i, y[i]] + delta
+        margins[y[i]] = 0  # loss doesn't consider j == y[i] -> let it be 0!
+        for idx, margin in enumerate(margins):  # len(margins) = K
+            if idx == y[i]:  # loss doesn't consider this case -> so grad won't have this
+                continue
+            if margin > 0:  # margin < 0 -> will be clipped -> no derivative
+                dtheta[:, idx] += X[i]		# X[i]'s shape: (1, d)
+                dtheta[:, y[i]] -= X[i]
 
+        margins = np.clip(margins, 0, None)
+        J += np.sum(margins)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dtheta.            #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
+        # Update the grad
 
+    J /= m
+    J += reg * np.sum(np.square(theta)) / 2.0
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dtheta.            #
+    # Rather that first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the     #
+    # loss is being computed. As a result you may need to modify some of the    #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+    dtheta = 1.0*dtheta/m
+    dtheta += reg * theta
 
-  return J, dtheta
+    return J, dtheta
 
 
 def svm_loss_vectorized(theta, X, y, reg):
-  """
-  Structured SVM loss function, vectorized implementation.
+    """
+    Structured SVM loss function, vectorized implementation.
 
-  Inputs and outputs are the same as svm_loss_naive.
-  """
-  J = 0.0
-  dtheta = np.zeros(theta.shape) # initialize the gradient as zero
-  delta = 1.0
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    J = 0.0
+    dtheta = np.zeros(theta.shape)  # initialize the gradient as zero
+    delta = 1.0
+    m = X.shape[0]
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the structured SVM loss, storing the    #
-  # result in variable J.                                                     #
-  # 8-10 lines of code                                                        #
-  #############################################################################
-  
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the structured SVM loss, storing the    #
+    # result in variable J.                                                     #
+    # 8-10 lines of code                                                        #
+    #############################################################################
+    pred = np.dot(X, theta)  # (m, k)
+    pred_correct = pred[np.arange(m), y].reshape((-1, 1))  # (m, 1)
+    margins = pred - pred_correct + delta  # (m, k)
+    margins = np.clip(margins, 0, None)
+    margins[np.arange(m), y] = 0
 
+    J = 1.0*np.sum(margins)/m + reg * np.sum(np.square(theta)) / 2.0
 
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+    #############################################################################
+    #                             END OF YOUR CODE                              #
+    #############################################################################
 
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the gradient for the structured SVM     #
+    # loss, storing the result in dtheta.                                       #
+    #                                                                           #
+    # Hint: Instead of computing the gradient from scratch, it may be easier    #
+    # to reuse some of the intermediate values that you used to compute the     #
+    # loss.                                                                     #
+    #############################################################################
+    # (m, k), also explicitly cast bool to int or it can't be used to subtract
+    margins_mask = (margins > 0).astype(float)
 
-  #############################################################################
-  # TODO:                                                                     #
-  # Implement a vectorized version of the gradient for the structured SVM     #
-  # loss, storing the result in dtheta.                                       #
-  #                                                                           #
-  # Hint: Instead of computing the gradient from scratch, it may be easier    #
-  # to reuse some of the intermediate values that you used to compute the     #
-  # loss.                                                                     #
-  #############################################################################
+    # For every row in margins:
+    # we will subtract X[i] for [margin>0] times
+    # add X[i] for [margin > 0] times
+    # shape: (m, k)
+    margins_mask[np.arange(m), y] -= np.sum(margins_mask, axis=1)
+    dtheta = np.dot(X.T, margins_mask) / m + reg * theta
 
-  #############################################################################
-  #                             END OF YOUR CODE                              #
-  #############################################################################
+    #############################################################################
+    #                             END OF YOUR CODE                              #
+    #############################################################################
 
-  return J, dtheta
+    return J, dtheta
