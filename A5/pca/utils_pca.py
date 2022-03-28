0cb835a5548138f966d1abbdac127a6ed68eaba9
Mikerpen22 <b04502053@ntu.edu.tw>
2022-03-28 16:36:43 -0500

    finish utils_pca.py

--- A5/pca/utils_pca.py
+++ A5/pca/utils_pca.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def pca(X):
     """
     Run PCA on dataset X
@@ -9,26 +10,28 @@ def pca(X):
     #########################################################
     #         YOUR CODE HERE                                #
     #########################################################
-    
+
     # compute the covariance of X and then use the
     # svd function to compute the eigenvectors and
     # eigenvalues of the covariance matrix
 
     # When computing the covariance remember to divide by
     # the number of rows in X
-    
-    
-
+    # np.cov() <- Each row represents a variable, and each column a single observation of all those variables
+    cov = np.cov(X.T)
+    U, S, V = np.linalg.svd(cov, full_matrices=False)
     ########################################################
     #           END YOUR CODE                              #
     ########################################################
-    return U,S,V
+    return U, S, V
+
 
 def feature_normalize(X):
     Xnorm = (X - X.mean(axis=0))/X.std(axis=0)
     return Xnorm, X.mean(axis=0), X.std(axis=0)
 
-def project_data(X,U,K):
+
+def project_data(X, U, K):
     """
     project_data computes the reduced data representation when projecting only 
     on to the top k eigenvectors
@@ -39,16 +42,15 @@ def project_data(X,U,K):
     #########################################################
     #         YOUR CODE HERE                                #
     #########################################################
-    
-    
-    
+    eigen_bases = U[:, 0:K]
+    Z = np.matmul(X,  eigen_bases)  # Projection <-> dot product
     ########################################################
     #           END YOUR CODE                              #
     ########################################################
     return Z
 
 
-def recover_data(Z,U,K):
+def recover_data(Z, U, K):
     """
     recover_data recovers an approximation of the original data when using the 
     projected principal axis U
@@ -60,12 +62,9 @@ def recover_data(Z,U,K):
     #########################################################
     #         YOUR CODE HERE                                #
     #########################################################
-    
-    
-
+    eigen_bases = U[:, 0:K]
+    X_rec = np.matmul(Z, eigen_bases.T)
     ########################################################
     #           END YOUR CODE                              #
     ########################################################
     return X_rec
-   
-    
